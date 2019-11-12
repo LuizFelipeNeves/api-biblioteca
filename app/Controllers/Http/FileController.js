978caa94a90ec52f588d6d0b7226f0cba5f67805
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-11-12 14:58:17 -0300

    Create a model and Controller for Files

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -1,15 +1,15 @@
+/* eslint-disable class-methods-use-this */
+
 'use strict';
 
-const File = use('App/Models/File')
-const Helpers = use('Helpers')
+const File = use('App/Models/File');
+const Helpers = use('Helpers');
 
 class FileController {
-  async show ({ params, response }) {
-    const file = await File.findBy('file', params.name)
-    if (file) return response.download(Helpers.tmpPath(`uploads/${file.file}`))
-    return response
-      .status(404)
-      .send({ error: { messsage: 'Arquivo não encontrado' } })
+  async show({ params, response }) {
+    const { file } = await File.findBy('file', params.name);
+    if (file) return response.download(Helpers.tmpPath(`uploads/${file}`));
+    return response.status(404).send([{ messsage: 'Arquivo não encontrado' }]);
   }
 
   /**
@@ -20,35 +20,33 @@ class FileController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
     try {
-      if (!request.file('file')) return
-
-      const upload = request.file('file', { size: '2mb' })
-      const fileName = `${Date.now()}.${upload.subtype}`
+      const upload = request.file('file', { size: '2mb' });
+      if (!upload) {
+        return response.status(500).send([{ messsage: 'Insira o arquivo ' }]);
+      }
+      const fileName = `${Date.now()}.${upload.subtype}`;
 
       await upload.move(Helpers.tmpPath('uploads'), {
-        name: fileName
+        name: fileName,
         // overwrite: true
-      })
+      });
 
-      if (!upload.moved()) {
-        throw upload.error()
-      }
+      if (!upload.moved()) throw upload.error();
 
-      const file = await File.create({
+      return File.create({
         file: fileName,
         name: upload.clientName,
         type: upload.type,
-        subtype: upload.subtype
-      })
-      return file
+        subtype: upload.subtype,
+      });
     } catch (error) {
       return response
         .status(error.status)
-        .send({ error: { messsage: 'Erro no upload de arquivo ' } })
+        .send([{ messsage: 'Erro no upload de arquivo ' }]);
     }
   }
 }
 
-module.exports = FileController
+module.exports = FileController;
--- app/Models/File.js
+++ app/Models/File.js
@@ -1,17 +1,15 @@
-'use strict';
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-const Env = use('Env')
+/* eslint-disable class-methods-use-this */
+const Model = use('Model');
+const Env = use('Env');
 
 class File extends Model {
-  static get computed () {
-    return ['url']
+  static get computed() {
+    return ['url'];
   }
 
-  getUrl ({ file }) {
-    return `${Env.get('APP_URL')}/files/${file}`
+  getUrl({ file }) {
+    return `${Env.get('APP_URL')}/files/${file}`;
   }
 }
 
-module.exports = File
+module.exports = File;
