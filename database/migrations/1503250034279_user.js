028654e7aa70425b9c83d0ab530944b6b0b840e9
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-11-12 14:59:55 -0300

    Update user model

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -1,30 +1,27 @@
-'use strict'
+'use strict';
 
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', table => {
-      table.increments()
-      table.string('first_name', 80).notNullable()
-      table.string('last_name', 80).notNullable()
+  up() {
+    this.create('users', (table) => {
+      table.increments();
+      table.string('full_name', 80).notNullable();
       table
         .string('email', 254)
         .notNullable()
-        .unique()
+        .unique();
 
-      // gender
-      // date of birth
-      table.string('password', 60).notNullable()
-      table.string('token')
-      table.timestamp('token_created_at')
-      table.timestamps()
-    })
+      table.string('password', 60).notNullable();
+      table.string('token');
+      table.timestamp('token_created_at');
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('users')
+  down() {
+    this.drop('users');
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;
